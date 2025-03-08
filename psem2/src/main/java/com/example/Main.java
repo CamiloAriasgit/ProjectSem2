f995a1ade914d8cab4ebe78557112fe8d9fe6681
SCAB <camiloab203@gmail.com>
2025-03-07 22:54:06 -0500

    primer cambio

---
+++ psem2/src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Jugador jugador1 = new Jugador("Pepe Guamo", 30, "defensa", 10, "las Máquinas");
+    Jugador jugador2 = new Jugador("Armando Paredes de Casas", 40, "delantero", 01, "Tigres rojos");
+
+    
+    jugador1.mostrarInformacion();
+    jugador2.mostrarInformacion();
+
+    
+    jugador1.setEdad(31);
+    jugador2.setEquipo("Tigres Naranjas");
+
+    
+    jugador1.mostrarInformacion();
+    jugador2.mostrarInformacion();
+    }
+}
